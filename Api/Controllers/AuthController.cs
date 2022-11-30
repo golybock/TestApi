0aba2d23ed5fc84fd765c5aeacd2ad6ee7b00507
golybock <teplovartem3094@gmail.com>
2022-11-30 21:39:47 +0300

    reorganized login to service

--- Api/Controllers/AuthController.cs
+++ Api/Controllers/AuthController.cs
@@ -24,49 +24,7 @@ namespace Api.Controllers
         [HttpPost, Route("login")]
         public IActionResult Login(string login, string password, string key)
         {
-            try
-            {
-                if (AuthDataIsNull(login, password, key))
-                    return BadRequest("Username and/or Password, key not specified");
-
-                if (KeyIsValid(key))
-                    return BadRequest("Key is not valid");
-                
-                if (_authService.ClientAuthDataValid(login, password))
-                {
-                    var secretKey = new SymmetricSecurityKey
-                        (Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
-                    var signinCredentials = new SigningCredentials
-                        (secretKey, SecurityAlgorithms.HmacSha256);
-                    var jwtSecurityToken = new JwtSecurityToken(
-                        issuer: _configuration["Jwt:Issuer"],
-                        audience: _configuration["Jwt:Audience"],
-                        claims: new List<Claim> {new (ClaimTypes.Email, login)},
-                        expires: DateTime.UtcNow.AddHours(100),
-                        signingCredentials: signinCredentials
-                    );
-                    return Ok(new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken));
-                }
-            }
-            catch(Exception ex)
-            {
-                return BadRequest
-                    ("An error occurred in generating the token" + ex);
-            }
-            return Unauthorized();
-        }
-
-        public bool AuthDataIsNull(string login, string pass, string key)
-        {
-            return string.IsNullOrEmpty(login) ||
-                   string.IsNullOrEmpty(pass) ||
-                   string.IsNullOrEmpty(key);
-        }
-
-        public bool KeyIsValid(string key)
-        {
-            // сравниваем секретный ключ с полученым
-            return _configuration["AppSettings:Secret"] == key;
+            return _authService.Login(login, password, key);
         }
         
     }
--- Api/Services/AuthService.cs
+++ Api/Services/AuthService.cs
@@ -1,6 +1,10 @@
-﻿using Api.DB;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Api.DB;
 using Api.Models.Client;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Api.Services;
 
@@ -14,17 +18,78 @@ public class AuthService
         _configuration = configuration;
         _productRepository = new ProductRepository(_configuration.GetConnectionString("ProductsAppCon"));
     }
-
-    public bool ClientAuthDataValid(string login, string password)
+    
+    public IActionResult Login(string login, string password, string key)
     {
-        OkObjectResult ok = _productRepository.GetClientByLogin(login) as OkObjectResult;
+        try
+        {
+            if (AuthDataIsNull(login, password, key))
+                return new BadRequestObjectResult("Username and/or Password, key not specified");
+
+            if (KeyIsValid(key))
+                return new BadRequestObjectResult("Key is not valid");
+
+            string token = GenerateToken(
+                new List<Claim> {new(ClaimTypes.Email, login)});
+            
+            if (ClientAuthDataValid(login, password))
+            {
+                return new OkObjectResult(token);
+            }
+        }
+        catch(Exception ex)
+        {
+            return new BadRequestObjectResult("An error occurred in generating the token" + ex);
+        }
+        return new UnauthorizedResult();
+    }
+
+    private bool ClientAuthDataValid(string login, string password)
+    {
+        OkObjectResult? ok = _productRepository.GetClientByLogin(login) as OkObjectResult;
         
-        var cl = ok.Value as Client;
+        var cl = ok?.Value as Client;
         
-        if (cl.Id != 0)
+        if (cl != null && cl.Id != 0)
         {
             return cl.Password == password;
         }
         return false;
     }
+
+    private bool AuthDataIsNull(string login, string pass, string key)
+    {
+        return string.IsNullOrEmpty(login) ||
+               string.IsNullOrEmpty(pass) ||
+               string.IsNullOrEmpty(key);
+    }
+
+    private bool KeyIsValid(string key)
+    {
+        // сравниваем секретный ключ с полученым
+        return _configuration["AppSettings:Secret"] == key;
+    }
+
+    private string GenerateToken(List<Claim> claims)
+    {
+        var secretKey = new SymmetricSecurityKey
+            (Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+                    
+        var signinCredentials = new SigningCredentials
+            (secretKey, SecurityAlgorithms.HmacSha256);
+
+        var issuer = _configuration["Jwt:Issuer"];
+
+        var audience = _configuration["Jwt:Audience"];
+                    
+        var jwtSecurityToken = new JwtSecurityToken(
+            issuer: issuer,
+            audience: audience,
+            claims: claims,
+            signingCredentials: signinCredentials
+        );
+        
+        return new JwtSecurityTokenHandler().WriteToken(jwtSecurityToken);
+    }
+    
 }
