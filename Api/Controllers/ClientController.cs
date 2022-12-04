8bb40fa5ed6530a9403f393c34c00766550f30ae
golybock <teplovartem3094@gmail.com>
2022-12-04 19:56:44 +0300

    refactored ClientController.cs

--- Api/Controllers/ClientController.cs
+++ Api/Controllers/ClientController.cs
@@ -18,63 +18,42 @@ public class ClientController : ControllerBase, IClientService
     [HttpGet("GetClientById")]
     public IActionResult GetClient(int id)
     {
-        if (id < 1)
-            return BadRequest("Id cannot be less than 1");
-        
         return _clientService.GetClient(id);
     }
     
     [HttpGet("GetClientByToken")]
     public IActionResult GetClient(string token)
     {
-        if (string.IsNullOrEmpty(token))
-            return BadRequest("Token cannot be empty");
-        
         return _clientService.GetClient(token);
     }
     
     [HttpGet("GetClientByLogin")]
     public IActionResult GetClientByLogin(string login)
     {
-        if (string.IsNullOrEmpty(login))
-            return BadRequest("Login cannot be empty");
-        
         return _clientService.GetClientByLogin(login);
     }
 
     [HttpGet("GetClientOrders")]
     public IActionResult GetClientOrders(int id)
     {
-        if (id < 1)
-            return BadRequest("Id cannot be less than 1");
-
         return _clientService.GetClientOrders(id);
     }
     
     [HttpPost("AddClient")]
     public IActionResult AddClient(Client client)
     {
-        if (client.Id < 1)
-            return BadRequest("Id cannot be less than 1");
-        
         return _clientService.AddClient(client);
     }
     
     [HttpPost("EditClient")]
     public IActionResult EditClient(Client client)
     {
-        if (client.Id < 1)
-            return BadRequest("Id cannot be less than 1");
-
         return _clientService.EditClient(client);
     }
     
     [HttpPost("DeleteClient")]
     public IActionResult DeleteClient(int id)
     {
-        if (id < 1)
-            return BadRequest("Id cannot be less than 1");
-        
         return _clientService.DeleteClient(id);
     }
 }
--- Api/Services/ClientService.cs
+++ Api/Services/ClientService.cs
@@ -15,36 +15,57 @@ public class ClientService : IClientService
     
     public IActionResult GetClient(int id)
     {
+        if (id < 1)
+            return new BadRequestObjectResult("Id cannot be less than 1");
+
         return _productRepository.GetClient(id);
     }
 
     public IActionResult GetClient(string token)
     {
+        if (string.IsNullOrEmpty(token))
+            return new BadRequestObjectResult("Token cannot be empty");
+
         return _productRepository.GetClient(token);
     }
 
     public IActionResult GetClientByLogin(string login)
     {
+        if (string.IsNullOrEmpty(login))
+            return new BadRequestObjectResult("Login cannot be empty");
+
         return _productRepository.GetClientByLogin(login);
     }
 
     public IActionResult GetClientOrders(int id)
     {
+        if (id < 1)
+            return new BadRequestObjectResult("Id cannot be less than 1");
+
         return _productRepository.GetClientOrders(id);
     }
 
     public IActionResult AddClient(Client client)
     {
+        if (client.Id < 1)
+            return new BadRequestObjectResult("Id cannot be less than 1");
+
         return _productRepository.AddClient(client);
     }
 
     public IActionResult EditClient(Client client)
     {
+        if (client.Id < 1)
+            return new BadRequestObjectResult("Id cannot be less than 1");
+
         return _productRepository.EditClient(client);
     }
     
     public IActionResult DeleteClient(int id)
     {
+        if (id < 1)
+            return new BadRequestObjectResult("Id cannot be less than 1");
+
         return _productRepository.DeleteClient(id);
     }
 }
